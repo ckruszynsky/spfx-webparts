cafc5638ade0f9ee40dc3625fb2ef59ee73124b3
Chad Kruszynsky <ckruszynsky@gmail.com>
2019-05-08 14:41:36 -0500

    moved private methods outside of component

--- examples/msgraph-get-profile-example/src/webparts/msGraphUserProfile/components/UserProfileCard/UserProfileCard.tsx
+++ examples/msgraph-get-profile-example/src/webparts/msGraphUserProfile/components/UserProfileCard/UserProfileCard.tsx
@@ -18,6 +18,38 @@ type UserProfileCardProps = {
   photoUrl: string;
 };
 
+
+const onRenderTertiaryText = (props: IPersonaProps): React.ReactElement<any> => {
+  if (props.tertiaryText) {
+    return (
+      <div>
+        <Icon iconName={"Mail"} className={"ms-JobIconExample"} /> {"  "}
+        <Link href={`mailTo:${props.tertiaryText}`}>{props.tertiaryText}</Link>
+      </div>
+    );
+  }
+};
+const onRenderOptionalText = (props: IPersonaProps): React.ReactElement<any> => {
+  if (props.optionalText) {
+    return (
+      <div>
+        <Icon iconName={"Phone"} className={"ms-JobIconExample"} /> {"  "}
+        <Link href={`tel:${props.optionalText}`}>{props.optionalText}</Link>
+      </div>
+    );
+  }
+};
+const onRenderSecondaryText = (props: IPersonaProps): JSX.Element => {
+  return (
+    <div>
+      <Icon iconName={"Suitcase"} className={"ms-JobIconExample"} />
+      {"  "}
+      {props.secondaryText}
+    </div>
+  );
+};
+
+
 export class UserProfileCard extends React.Component<UserProfileCardProps, {}> {
   public render(): React.ReactElement<UserProfileCardProps> {
     const { name, jobTitle, emailAddress, phoneNumber, size, photoUrl } = this.props;
@@ -32,41 +64,13 @@ export class UserProfileCard extends React.Component<UserProfileCardProps, {}> {
       <Persona
         {...userPersona}
         text={name}
-        onRenderTertiaryText={this._onRenderTertiaryText}
-        onRenderSecondaryText={this._onRenderSecondaryText}
-        onRenderOptionalText={this._onRenderOptionalText}
+        onRenderTertiaryText={onRenderTertiaryText}
+        onRenderSecondaryText={onRenderSecondaryText}
+        onRenderOptionalText={onRenderOptionalText}
         imageUrl={photoUrl}
       />
     );
   }
 
-  private _onRenderTertiaryText = (props: IPersonaProps): React.ReactElement<any> => {
-    if (props.tertiaryText) {
-      return (
-        <div>
-          <Icon iconName={"Mail"} className={"ms-JobIconExample"} /> {"  "}
-          <Link href={`mailTo:${props.tertiaryText}`}>{props.tertiaryText}</Link>
-        </div>
-      );
-    }
-  };
-  private _onRenderOptionalText = (props: IPersonaProps): React.ReactElement<any> => {
-    if (props.optionalText) {
-      return (
-        <div>
-          <Icon iconName={"Phone"} className={"ms-JobIconExample"} /> {"  "}
-          <Link href={`tel:${props.optionalText}`}>{props.optionalText}</Link>
-        </div>
-      );
-    }
-  };
-  private _onRenderSecondaryText = (props: IPersonaProps): JSX.Element => {
-    return (
-      <div>
-        <Icon iconName={"Suitcase"} className={"ms-JobIconExample"} />
-        {"  "}
-        {props.secondaryText}
-      </div>
-    );
-  };
+  
 }
